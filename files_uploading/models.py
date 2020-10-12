5f02e25397b3b21883576642f36d1b878537f056
Vladimir <shitov@biocad.ru>
2020-10-12 20:07:06 +0700

    Change upload folder for VCF files

--- files_uploading/models.py
+++ files_uploading/models.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from django.core.validators import FileExtensionValidator
 from django.db import models
 
@@ -6,6 +8,7 @@ from .validators import check_vcf_format
 
 class VCFFile(models.Model):
     file = models.FileField(
+        upload_to=Path('raw_data/vcf/'),
         validators=[
             check_vcf_format,
             FileExtensionValidator(allowed_extensions=['vcf', 'vcf.gz', 'bcf']),
