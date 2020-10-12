e81bdfabd7181ccaaa3539f968197572ca8ebe93
Vladimir <shitov@biocad.ru>
2020-10-12 15:49:32 +0700

    Add FileExtensionValidator

--- files_uploading/models.py
+++ files_uploading/models.py
@@ -1,7 +1,13 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 from .validators import check_vcf_format
 
 
 class VCFFile(models.Model):
-    file = models.FileField(validators=[check_vcf_format])
+    file = models.FileField(
+        validators=[
+            check_vcf_format,
+            FileExtensionValidator(allowed_extensions=['vcf', 'vcf.gz', 'bcf']),
+        ]
+    )
