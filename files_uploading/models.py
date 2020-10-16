961273e34bd7a67f7b8c264f74ed8aad444c9385
Vladimir <shitov@biocad.ru>
2020-10-16 20:00:15 +0700

    Reformat models

--- files_uploading/models.py
+++ files_uploading/models.py
@@ -20,30 +20,43 @@ class VCFFile(models.Model):
 
 
 class Allele(models.Model):
-    variant = models.CharField(max_length=15, blank=False, primary_key=True)
-
-
-class SNP(models.Model):
-    name = models.CharField(max_length=255, primary_key=True)
-    reference_allele = models.ForeignKey(to=Allele, on_delete=models.CASCADE, related_name='REF')
-    alternative_allele = models.ForeignKey(to=Allele, on_delete=models.CASCADE, related_name='ALT')
+    genotype = models.CharField(max_length=15, blank=False, primary_key=True)
 
 
 class Chromosome(models.Model):
+
+    class NamesMapper:
+        names_to_number_map = {f'chr{i}': i for i in range(1, 23)}
+        names_to_number_map.update(
+            {'X': 23, 'chrX': 23, 'Y': 24, 'chrY': 24, 'XY': 25, 'chrXY': 25, 'chrM': 26, 'MT': 26}
+        )
+
+        @classmethod
+        def name_to_number(cls, name):
+            return cls.names_to_number_map[name]
+
     number = models.SmallIntegerField(primary_key=True)
 
 
-class AllelesRecord(models.Model):
-    record = models.CharField(max_length=15, blank=False, primary_key=True)
-
-
-class Variant(models.Model):
-    chromosome_number = models.ForeignKey(to=Chromosome, on_delete=models.CASCADE)
+class SNP(models.Model):
+    name = models.CharField(max_length=255, primary_key=True, blank=True)
+    chromosome = models.ForeignKey(to=Chromosome, on_delete=models.CASCADE)
     position = models.IntegerField(blank=False)
-    alleles_record = models.ForeignKey(
-        to=AllelesRecord, on_delete=models.SET_NULL, null=True, blank=True
+    reference_allele = models.ForeignKey(
+        to=Allele, on_delete=models.CASCADE, related_name='ref_to_snp'
     )
-    snp = models.ForeignKey(to=SNP, on_delete=models.SET_NULL, null=True, blank=True)
+    alternative_alleles = models.ManyToManyField(to=Allele, related_name='alt_to_snp')
+
+
+class AllelesRecord(models.Model):
+
+    @staticmethod
+    def from_tuple(alleles):
+        if alleles == (1, 0):
+            return '0/1'
+        return '/'.join(alleles)
+
+    record = models.CharField(max_length=15, blank=False, primary_key=True)
 
 
 class Nationality(models.Model):
@@ -66,7 +79,7 @@ class Sample(models.Model):
         OTHER = 'O', _('Other')
         UNDEFINED = 'U', _('Undefined')
 
-    cypher = models.CharField(max_length=255)
+    cypher = models.CharField(max_length=255, primary_key=True)
     gender = models.CharField(max_length=15, choices=Gender.choices, default=Gender.UNDEFINED)
     nationality = models.ForeignKey(
         to=Nationality, on_delete=models.SET_NULL, null=True, blank=True
@@ -78,3 +91,12 @@ class Sample(models.Model):
         to=YHaplogroup, on_delete=models.SET_NULL, null=True, blank=True
     )
     vcf_file = models.ForeignKey(to=VCFFile, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Variant(models.Model):
+    alleles_record = models.ForeignKey(
+        to=AllelesRecord, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    alleles = models.ManyToManyField(to=Allele)
+    sample = models.ForeignKey(to=Sample, on_delete=models.CASCADE)
+    snp = models.ForeignKey(to=SNP, on_delete=models.SET_NULL, null=True, blank=True)
