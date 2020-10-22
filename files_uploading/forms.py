8cd760db2a9b65f91b0710454ad302fd7ea2afba
Vladimir <shitov@biocad.ru>
2020-10-22 20:50:31 +0700

    Change SNPSearchForm's ancestor from ModelForm to Form

--- files_uploading/forms.py
+++ files_uploading/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.db import transaction
 from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
@@ -5,7 +6,7 @@ from loguru import logger
 from pysam import VariantFile
 from typing import Optional
 
-from .models import VCFFile, SNP
+from .models import Allele, Chromosome, VCFFile, SNP
 from .utils import are_samples_empty, parse_samples, save_record_to_db
 from .types import SamplesDict
 
@@ -41,13 +42,19 @@ class VCFFileForm(ModelForm):
                     save_record_to_db(record=record, samples=samples)
 
 
-class SNPSearchForm(ModelForm):
-    class Meta:
-        model = SNP
-        fields = ["chromosome", "position", "reference_allele", "alternative_allele"]
-        labels = {
-            "chromosome": _("Chromosome"),
-            "position": _("Position"),
-            "reference_allele": _("Reference allele"),
-            "alternative_allele": _("Alternative allele"),
-        }
+class SNPSearchForm(forms.Form):
+    chromosome = forms.ModelChoiceField(
+        queryset=Chromosome.objects.all(), empty_label=None, required=True, label=_('Chromosome')
+    )
+    position = forms.IntegerField(
+        min_value=0, max_value=3.5*10**9, required=True, label=_('Position')
+    )
+    allele_1 = forms.ModelChoiceField(
+        queryset=Allele.objects.all(), empty_label=None, required=True, label=_('The first allele')
+    )
+    allele_2 = forms.ModelChoiceField(
+        queryset=Allele.objects.all(),
+        empty_label=None,
+        required=True,
+        label=_("The second allele"),
+    )
