047295c874fbf69c2328e4b7cf7ab4569b90a886
Vladimir <shitov@biocad.ru>
2020-10-28 17:23:41 +0700

    Add SNP description to search_snp_form

--- files_uploading/views.py
+++ files_uploading/views.py
@@ -4,8 +4,8 @@ from loguru import logger
 
 from .forms import VCFFileForm, SNPSearchForm
 from .models import VCFFile, Sample
-from .utils import get_samples_from_snp
-from .types import SamplesSimilarityTable
+from .utils import get_samples_from_snp, get_snp_from_snp_search_form
+from .types import VariantDict, SamplesSimilarityTable
 
 
 def index(request):
@@ -66,7 +66,10 @@ def snp_search_form(  # TODO: validator
         if form.is_valid():
             logger.success('Form is valid, returning success')
             samples: SamplesSimilarityTable = get_samples_from_snp(request.POST)
-            return render(request, result_template, {'table': samples})
+            snp_description: VariantDict = get_snp_from_snp_search_form(request.POST)
+            return render(
+                request, result_template, {'table': samples, 'description': snp_description}
+            )
         else:
             logger.warning('Form is not valid')
             return render(request, form_template, {'form': form})
