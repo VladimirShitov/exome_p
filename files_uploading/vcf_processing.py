8c1df6db812308bf95dbca0ec14ee9f011547aef
Vladimir <shitov@biocad.ru>
2020-11-06 17:35:17 +0700

    Add VCFRecord and VCFFile

---
+++ files_uploading/vcf_processing.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from typing import List
+
+
+class VCFRecord:
+    def __init__(
+            self,
+            chromosome,
+            position,
+            sample,
+            sample_indexes,
+            ref,
+            alts: List[str],
+            id_=".",
+            quality=".",
+            filter_=".",
+            format_='.',
+            info=".",
+    ):
+        self.chromosome = chromosome
+        self.position = position
+        self.id_ = id_
+        self.ref = ref
+        self.alts = alts
+        self.quality = quality
+        self.filter_ = filter_
+        self.info = info
+        self.format_ = format_
+        self.sample_indexes = sample_indexes
+        self.sample = sample
+
+    def __str__(self):
+        return f'{self.chromosome}\t{self.position}\t{self.id_}\t{self.ref}' \
+               f'\t{",".join(self.alts)}\t{self.quality}\t{self.filter_}' \
+               f'\t{self.info}\t{self.format_}\t{self.sample_indexes}'
+
+
+class VCFFile:
+    header = [
+        '##fileformat=VCFv4.3',
+        '##source=exome_p',
+        '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">'
+    ]
+    columns = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT']
+
+    def __init__(self, sample):
+        self.sample = sample
+
+        self.columns.append(sample)
+        self.records = []
+
+    def add_record(self, record: VCFRecord):
+        self.records.append(record)
+
+    def save(self, file_path: Path):
+        with open(file_path, 'w') as f:
+            f.write('\n'.join(self.header) + '\n')
+            f.write('\t'.join(self.columns) + '\n')
+            for record in self.records:
+                f.write(str(record) + '\n')
