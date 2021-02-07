c5a2049fdd458ee41a37e70f43f2bb283285937d
Vladimir <shitov@biocad.ru>
2021-02-08 01:02:00 +0700

    Add vcf/save and vcf/nationality

--- exome_p/urls.py
+++ exome_p/urls.py
@@ -17,23 +17,22 @@ from django.contrib import admin
 from django.urls import path
 
 from nationality_prediction.views import upload_genotype_for_prediction
-from vcf_uploading.views import (
-    index,
-    samples_list,
-    snp_search_form,
-    vcf_file_download,
-    vcf_file_upload,
-    vcf_files_list,
-)
+import vcf_uploading.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name="index"),
-    path("file/vcf/", vcf_file_upload, name="upload"),
-    path("file/vcf/list", vcf_files_list, name="vcf_list"),
-    path("file/vcf/<int:file_id>", vcf_file_download, name="vcf_file"),
-    path("vcf/sample/list", samples_list, name="samples_list"),
-    path("snp/search", snp_search_form, name="snp_search"),
+    path("", vcf_uploading.views.index, name="index"),
+    path("file/vcf/", vcf_uploading.views.vcf_file_upload, name="upload"),
+    path("file/vcf/list", vcf_uploading.views.vcf_files_list, name="vcf_list"),
+    path("file/vcf/<int:file_id>", vcf_uploading.views.vcf_file_download, name="vcf_file"),
+    path("file/vcf/<int:file_id>/save", vcf_uploading.views.save_vcf, name="save_vcf"),
+    path(
+        "file/vcf/<int:file_id>/nationality",
+        vcf_uploading.views.predict_nationality_from_vcf,
+        name="predict_nationality_from_vcf"
+    ),
+    path("vcf/sample/list", vcf_uploading.views.samples_list, name="samples_list"),
+    path("snp/search", vcf_uploading.views.snp_search_form, name="snp_search"),
     path(
         "nationality/predict",
         upload_genotype_for_prediction,
