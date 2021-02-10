8648864bb837f1c8875096d5484c7786bf038175
Vladimir <shitov@biocad.ru>
2021-02-11 01:58:36 +0700

    Add DB schema

--- short_tandem_repeats/models.py
+++ short_tandem_repeats/models.py
@@ -1,3 +1,23 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+
+class STRFile(models.Model):
+    file = models.FileField(
+        upload_to="raw_data/str/",
+        validators=FileExtensionValidator(allowed_extensions=["xlsx"])
+    )
+
+
+class Sample(models.Model):
+    cypher = models.CharField(max_length=255, primary_key=True)
+
+
+class STRRegion(models.Model):
+    title = models.CharField(max_length=255, primary_key=True)
+
+
+class ShortTandemRepeat(models.Model):
+    sample = models.ForeignKey(to=Sample, on_delete=models.CASCADE)
+    region = models.ForeignKey(to=STRRegion, on_delete=models.CASCADE)
+    n_repeats = models.IntegerField()
