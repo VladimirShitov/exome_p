8d4c4d89726afaefce1814852702c9dfdb259d44
Vladimir Shitov <vladimirshitov98@gmail.com>
2021-11-15 02:21:07 +0700

    Add string representation for STRRegion

--- short_tandem_repeats/models.py
+++ short_tandem_repeats/models.py
@@ -16,6 +16,9 @@ class Sample(models.Model):
 class STRRegion(models.Model):
     title = models.CharField(max_length=255, primary_key=True)
 
+    def __str__(self):
+        return self.title
+
 
 class ShortTandemRepeat(models.Model):
     sample = models.ForeignKey(to=Sample, on_delete=models.CASCADE)
