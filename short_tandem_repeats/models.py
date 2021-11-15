3403f138e7047fbe5e8cb7c96737772e53fbcd83
Vladimir Shitov <vladimirshitov98@gmail.com>
2021-11-15 13:26:24 +0700

    Parse STR table to database

--- short_tandem_repeats/models.py
+++ short_tandem_repeats/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import FileExtensionValidator
 from django.db import models
 import pandas as pd
+from loguru import logger
 
 
 class Sample(models.Model):
@@ -36,6 +37,21 @@ class STRFile(models.Model):
     def save_to_db(self):
         df = pd.read_excel(self.file.path, engine="openpyxl")
 
-        for region in df.columns[1:]:
-            db_record, _ = STRRegion.objects.get_or_create(title=region)
-            db_record.save()
+        logger.info("Saving df to the database")
+        logger.debug("df shape: {}", df.shape)
+
+        for idx, row in df.iterrows():
+            logger.debug("Saving row {}", idx)
+
+            db_sample, _ = Sample.objects.get_or_create(cypher=row["Sample"])
+            db_sample.save()
+
+            for region in df.columns[1:]:
+                db_region, _ = STRRegion.objects.get_or_create(title=region)
+                db_region.save()
+
+                db_n_repeats, _ = NRepeats.objects.get_or_create(n_repeats=str(row[region]))
+
+                repeat, _ = ShortTandemRepeat.objects.get_or_create(
+                    sample=db_sample, region=db_region, n_repeats=db_n_repeats)
+                repeat.save()
