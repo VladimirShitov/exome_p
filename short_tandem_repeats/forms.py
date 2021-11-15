5b9a0a05134045e23c67f85ac801d11124e8c483
Vladimir Shitov <vladimirshitov98@gmail.com>
2021-11-15 13:28:05 +0700

    Made List text widget for NRepeats in STRSearchForm

--- short_tandem_repeats/forms.py
+++ short_tandem_repeats/forms.py
@@ -2,7 +2,32 @@ from django import forms
 from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 
-from short_tandem_repeats.models import STRFile, ShortTandemRepeat, STRRegion
+from loguru import logger
+
+from short_tandem_repeats.models import STRFile, ShortTandemRepeat, STRRegion, NRepeats
+
+
+class ListTextWidget(forms.TextInput):
+    """Widget that allows to choose from variants or to type one"""
+    def __init__(self, data_list, name, *args, **kwargs):
+        super(ListTextWidget, self).__init__(*args, **kwargs)
+
+        self.name = name
+        self._list = data_list
+        self.attrs.update({"list": f"list__{self.name}"})
+
+        logger.debug("ListTextWidget attrs: {}", self.attrs)
+
+    def render(self, name, value, attrs=None, renderer=None):
+        text_html = super(ListTextWidget, self).render(name, value, attrs=attrs)
+        data_list = f'<datalist id="list__{self.name}">'
+
+        for item in self._list:
+            data_list += f'<option value="{item}">'
+
+        data_list += "</datalist>"
+
+        return text_html + data_list
 
 
 class STRFileForm(ModelForm):
@@ -19,6 +44,17 @@ class STRSearchForm(forms.Form):
         required=True,
         label=_("Region"),
     )
-    n_repeats = forms.IntegerField(
-        min_value=0, max_value=3.5 * 10 ** 5, required=True, label=_("Number of repeats")
+    n_repeats = forms.ModelChoiceField(
+        queryset=NRepeats.objects.all(),
+        empty_label=None,
+        required=True,
+        label=_("Number of repeats"),
     )
+
+    def __init__(self, *args, **kwargs):
+        super(STRSearchForm, self).__init__()
+
+        repeats_list = kwargs.pop("repeats_list")
+
+        self.fields["n_repeats"].widget = ListTextWidget(data_list=repeats_list,
+                                                         name="repeats-list")
